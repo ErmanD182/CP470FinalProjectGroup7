7be7743480e010a5a469fc9052b510ed38143992
Erman <dins4040@mylaurier.ca>
2019-10-28 16:40:11 -0400

    erman button opens ErmanLoginActivity

---
+++ app/src/main/java/com/example/cp470finalprojectgroup7/Erman/ErmanLoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.cp470finalprojectgroup7.Erman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cp470finalprojectgroup7.R;
+
+public class ErmanLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erman_login);
+
+    }
+}
--- app/src/main/java/com/example/cp470finalprojectgroup7/MainActivity.java
+++ app/src/main/java/com/example/cp470finalprojectgroup7/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.cp470finalprojectgroup7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.cp470finalprojectgroup7.Erman.ErmanLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+Button ermanButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ermanButton = (Button)findViewById(R.id.Ermanbutton);
+        ermanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ErmanLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
